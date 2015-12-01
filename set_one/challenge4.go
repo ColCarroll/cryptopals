91b4857abd3f54d07ae8e3231ba3e10b381a491b
Colin Carroll <colin@Colins-MBP.andover.edu>
2015-11-30 22:48:34 -0500

    weekend work

--- set_one/challenge4.go
+++ set_one/challenge4.go
@@ -1,3 +1,10 @@
+/*Detect single-character XOR
+One of the 60-character strings in this file has been encrypted by single-character XOR.
+
+Find it.
+
+(Your code from #3 should help.)
+*/
 package set_one
 
 import (
